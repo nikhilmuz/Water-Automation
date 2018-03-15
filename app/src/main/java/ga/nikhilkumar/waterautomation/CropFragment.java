f30aa3aefbfc64510516414af7c98e2c4ef69a52
Ankit Raj <mrankitraj@gmail.com>
2018-03-16 01:04:50 +0530

    Crop Fragment Improvement and validations

--- app/src/main/java/ga/nikhilkumar/waterautomation/CropFragment.java
+++ app/src/main/java/ga/nikhilkumar/waterautomation/CropFragment.java
@@ -23,6 +23,8 @@ import com.android.volley.toolbox.Volley;
 public class CropFragment extends Fragment {
 String ip=Vars.ip;
 EditText min,max;
+String minstr="",maxstr="";
+int minint,maxint;
 Button setminmax;
     public CropFragment() {
         // Required empty public constructor
@@ -40,9 +42,25 @@ Button setminmax;
         setminmax.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendRequest("http://"+ip+"/");
-                Toast.makeText(getActivity(), String.valueOf(min.getText())+","+String.valueOf(max.getText())+Vars.ip, Toast.LENGTH_SHORT).show();
-            }
+                if (!(String.valueOf(min.getText()).equals("")||String.valueOf(max.getText()).equals(""))) {
+                    minint = Integer.parseInt(String.valueOf(min.getText()));
+                    maxint = Integer.parseInt(String.valueOf(max.getText()));
+                    if(minint<101&&maxint<101&&minint<maxint) {
+                        if (minint<10) minstr="00"+minint;
+                        else if (minint<100) minstr="0"+minint;
+                        if (maxint<10) maxstr="00"+maxint;
+                        else if (maxint<100) maxstr="0"+maxint;
+                        Toast.makeText(getActivity(), "Setting Parameters!!!", Toast.LENGTH_SHORT).show();
+                        sendRequest("http://" + ip + "/mm"+minstr+","+maxstr);
+                    }
+                    else {
+                        Toast.makeText(getActivity(), "Check Inputs!!!", Toast.LENGTH_SHORT).show();
+                    }
+                    }
+                else {
+                    Toast.makeText(getActivity(), "Fill all fields First!!!", Toast.LENGTH_SHORT).show();
+                }
+                }
         });
         return v;
     }
